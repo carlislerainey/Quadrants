cbc5232338a95f682ec0b5857287b5d51c2a8c34
Carlisle Rainey <carlislerainey@gmail.com>
2014-02-27 06:05:52 -0500

    change the hi value used to compute first different from 9 to the max (9.03) for the lnct plot

--- R_Code/plot_lnct.R
+++ R_Code/plot_lnct.R
@@ -95,7 +95,7 @@ png("Manuscript/Hypothesis_Tests/Figures/lnct.png", height = 400, width = 600)
 par(mfrow = c(2,2), mar = rep(0.75, 4), oma = c(3,3,1,1), family = "serif")
 
 # vectors for computing substantive effects and axis notation
-original.scale <- xticklab0 <- seq(0, 9, length.out = 10)
+original.scale <- xticklab0 <- seq(0, max(d$lnct), length.out = 10)
 rescaled <- xat0 <- (xticklab0 - mean(d$lnct))/(2*sd(d$lnct))
 lo <- rescaled[1]
 hi <- rescaled[length(xat0)]
